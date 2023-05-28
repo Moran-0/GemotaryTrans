a162c92c3bfbf42371623a8c9208615e6cd6a31c
Moran-0 <2857599224@qq.com>
2023-05-28 09:11:08 +0800

    Revert "添加项目文件。"
    This reverts commit 81987b2cff77a11d24b9a42d660a5fcd19b0d009.

--- StdAfx.cpp
+++
@@ -1,8 +0,0 @@
-#include "pch.h"
-#include "StdAfx.h"
-// stdafx.cpp : source file that includes just the standard includes
-//	test.pch will be the pre-compiled header
-//	stdafx.obj will contain the pre-compiled type information
-
-
-
