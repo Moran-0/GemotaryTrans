2a49c609ca28fea957f546156ed44aec201792b7
Moran-0 <2857599224@qq.com>
2023-05-28 09:13:17 +0800

    Revert "Revert "添加项目文件。""
    This reverts commit a162c92c3bfbf42371623a8c9208615e6cd6a31c.

---
+++ GemotaryTrans/GemotaryTransView.h
@@ -0,0 +1,56 @@
+﻿
+// GemotaryTransView.h: CGemotaryTransView 类的接口
+//
+
+#pragma once
+
+
+class CGemotaryTransView : public CView
+{
+protected: // 仅从序列化创建
+	CGemotaryTransView() noexcept;
+	DECLARE_DYNCREATE(CGemotaryTransView)
+
+// 特性
+public:
+	CGemotaryTransDoc* GetDocument() const;
+
+// 操作
+public:
+
+// 重写
+public:
+	virtual void OnDraw(CDC* pDC);  // 重写以绘制该视图
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+	virtual BOOL OnPreparePrinting(CPrintInfo* pInfo);
+	virtual void OnBeginPrinting(CDC* pDC, CPrintInfo* pInfo);
+	virtual void OnEndPrinting(CDC* pDC, CPrintInfo* pInfo);
+
+// 实现
+public:
+	virtual ~CGemotaryTransView();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	afx_msg void OnFilePrintPreview();
+	afx_msg void OnRButtonUp(UINT nFlags, CPoint point);
+	afx_msg void OnContextMenu(CWnd* pWnd, CPoint point);
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnMDraw();
+	afx_msg void OnProJect();
+	afx_msg void DrawRoll();
+};
+
+#ifndef _DEBUG  // GemotaryTransView.cpp 中的调试版本
+inline CGemotaryTransDoc* CGemotaryTransView::GetDocument() const
+   { return reinterpret_cast<CGemotaryTransDoc*>(m_pDocument); }
+#endif
+
